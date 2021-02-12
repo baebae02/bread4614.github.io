3ad0d786c86eeb68becdb4023be168612366fff5
Dodolist <dodolist@naver.com>
2021-02-13 02:58:07 +0900

    Complete 25%

--- clock.js
+++ clock.js
@@ -8,10 +8,8 @@ function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  const seconds = date.getSeconds();
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    minutes < 10 ? `0${minutes}` : minutes}`;
   clockAMPM.innerText= `${hours > 11 ? `PM` : 'AM'}`;
 }
 
